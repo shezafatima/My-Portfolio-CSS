c765adecb2b91876115ebf8c0543beb20904fd42
shezafatima <sohailejaz058@gmail.com>
2025-02-04 17:47:22 +0500

    first commit

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,59 +1,8 @@
-// import Link from "next/link";
-// import React from "react";
-// import { BsFacebook } from "react-icons/bs";
-// import { BsYoutube } from "react-icons/bs";
-// import { BsInstagram } from "react-icons/bs";
-// import { BsLinkedin } from "react-icons/bs";
-// import { BsGithub } from "react-icons/bs";
 
-// const Footer = () => {
-//   return (
-//     <div id="Footer" className="bg-purple-400">
-//       <footer className=" body-font bg-fixed ">
-//         <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col ">
-//           <a className="flex title-font font-medium items-center md:justify-start justify-center">
-//             <span className="ml-3 text-xl font-bold  font-style:italic text-purple-800 sm:6xl">
-//             Sheza Fatima
-//             </span>
-//           </a>
-//           <p className="text-sm text-gray-700 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-//             © 2024 Sheza Fatima.Created by Sheza Fatima
-//           </p>
-//           <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-//          <Link 
-//          target="_blank"
-//          href={"https://www.facebook.com/profile.php?id=61566351903578&mibextid=ZbWKwL"} className="ml-3 text-gray-900 text-xl hover:text-purple-800">
-//          <BsFacebook /></Link>
-//          <Link
-//          target="_blank"
-//          href={"https://www.youtube.com/@shizacrafts7250"}
-         
-//          className="ml-3 text-gray-900 text-xl hover:text-purple-800">
-//          <BsYoutube />
-//          </Link>
-//          <Link 
-//           target="_blank"
-//          href={"https://www.instagram.com/1824digital_sf?igsh=ZGUzMzM3NWJiOQ=="} className="ml-3 text-gray-900 text-xl  hover:text-purple-800">
-//          <BsInstagram />
-//          </Link>
-//          <Link target="_blank"
-//          href={"https://github.com/shezafatima"} className="ml-3 hover:text-purple-800 text-xl text-gray-900">
-//           <BsGithub />
-//          </Link>
-//          <a className="ml-3 text-gray-900 text-xl hover:text-purple-800">
-//          <BsLinkedin />
-//          </a>
-//           </span>
-//         </div>
-//       </footer>
-//     </div>
-//   );
-// };
-// export default Footer;
 import Link from "next/link";
 import React from "react";
 import { BsFacebook, BsYoutube, BsInstagram, BsLinkedin, BsGithub } from "react-icons/bs";
-import "../../styles/footer.css"; // Adjust the path if necessary
+import "../../styles/footer.css"; 
 
 const Footer = () => {
   return (
--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,79 +1,31 @@
-"use client"
-import '../../styles/background.css'
+
+"use client";
 import Typewriter from 'typewriter-effect';
-const Hero=()=>{
-    return(
-        <div id='Hero'>
-            <section className=" backgroundImage body-font items-center hidden lg:block md:block ">
-                
-  <div className=" container mx-auto flex content-center px-5 py-24 items-center justify-center flex-col">
-    <div className='flex'>
-
-      <h1 className="title-font sm:text-8xl sm:mr-4 sm:mt-20 font-bold text-3xl  mb-4 drop-shadow-lg text-white">
-        Hi, it's  
-      </h1>
-      <h1 className="title-font sm:text-8xl sm:mt-20 font-bold text-3xl sm:ml:2 mb-4 drop-shadow-lg text-purple-800 ">
-        Sheza
-      </h1>
-    </div>
-      <div className='flex  text-7xl mt-6 drop-shadow-lg'>
-      <div className='  text-purple-600 font-extrabold'>
-        I'm a
-      </div>
-    <div className="text-white font-bold sm:ml-5  ">
-    <Typewriter
-  options={{
-    strings: ['Webdeveloper', 'Graphic designer','Web Designer'],
-    autoStart: true,
-    loop: true,
-  }}
-/>
-      </div>
-      
-      </div>
-     
-  </div>
-</section>
-<section className=" backgroundImage body-font items-center lg:hidden md:hidden">
-                
-  <div className=" container mx-auto flex flex-wrap content-center px-5 py-24 items-center justify-center flex-col">
-    <div className='flex'>
-
-      <h1 className="title-font mr-3 mt-40 font-bold text-5xl  mb-4 drop-shadow-lg text-white">
-        Hi, it's  
-      </h1>
-      <h1 className="title-font mt-40 font-bold text-5xl sm:ml:2 mb-4 drop-shadow-lg text-purple-800 ">
-        Sheza
-      </h1>
-    </div>
-    <h1 className="title-font mt-4 font-bold text-4xl  mb-4 drop-shadow-lg text-purple-800 ">
-        I'm a
-      </h1>
-      <div className='flex  text-3xl  drop-shadow-lg'>
-      
-    <div className="text-white font-bold   ">
-    <Typewriter
-  options={{
-    strings: ['Webdeveloper', 'Graphic designer','Web Designer'],
-    autoStart: true,
-    loop: true,
-  }}
-/>
-      </div>
-      
-      </div>
-      {/* <div className="flex justify-center">
-        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-          Button
-        </button>
-        <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-          Button
-        </button>
-    </div> */}
-  </div>
-</section>
-
+import '../../styles/hero.css'
+const Hero = () => {
+  return (
+    <div id="Hero">
+      <section className="hero-section">
+        <div className="hero-container">
+          <div className="hero-text">
+            <h1 className="hero-heading">
+              Hi, it's <span className="hero-name">Sheza</span>
+            </h1>
+            <div className="typewriter-container">
+              <span>I'm a </span>
+              <Typewriter
+                options={{
+                  strings: ['Web Developer', 'Graphic Designer', 'Web Designer'],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+            </div>
+          </div>
         </div>
-    )
-}
-export default Hero
+      </section>
+    </div>
+  );
+};
+
+export default Hero;
